c73e3c2ae612bbd2fc4c83c669b99e4fae0f9b34
Mayank Sinha <mayank.sinha022@gmail.com>
2014-02-10 14:06:30 -0500

    Initial Commit

---
+++ ToDoList/TestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestAppDelegate.h
+//  ToDoList
+//
+//  Created by Mayank on 2/10/14.
+//  Copyright (c) 2014 Mayank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Mayank on 2/10/14.
+//  Copyright (c) 2014 Mayank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestAppDelegate class]));
+    }
+}
